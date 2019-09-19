7591f274e3c5ff1523c6ae8be89a5c3df5989cf2
zouzhanhao <zouzhanhao@zouzhanhaodeMacBook-Pro.local>
2019-09-19 12:53:06 +0800

    edit

--- test/Test.php
+++
@@ -1,7 +0,0 @@
-<?php
-require "../vendor/autoload.php";
-
-use Log\Log;
-
-#注意目录权限
-Log::log('test','filename','title');
---
+++ test/demo.php
@@ -0,0 +1,7 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use Log\Log;
+
+Log::log('text', 'filename', 'biaoti');
